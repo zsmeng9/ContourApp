804703eeb7e6940eb94ebeed82f7374a11783a89
Allan Meng <allanmeng@Allans-MacBook-Pro.local>
2016-12-10 21:26:36 -0500

    bind app to port for deployment

--- contour.py
+++ contour.py
@@ -1,5 +1,6 @@
 import json
 import math
+import os
 
 from flask import Flask, render_template, request
 
@@ -95,4 +96,6 @@ def cardCalibration(deltaTuple):
 
 
 if __name__ == "__main__":
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
